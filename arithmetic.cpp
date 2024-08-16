fcc9a141ec1b9efe91f453a2e1637d91c056d9c8
rakib-siddiki <rmrakib901@gmail.com>
2024-08-17 05:46:32 +0600

    first commit

---
+++ arithmetic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+    int  num,num2;
+    cout << "eneter first a number :";
+    cin >> num;
+    cout << "enter your second number :";
+    cin >> num2;
+    cout << "your enterd numbers sum is =" << num + num2 <<endl;
+    cout << "your enterd numbers multipication is =" << num * num2 <<endl;
+    cout << "your enterd numbers divide is =" << num / num2 <<endl;
+    cout << "your enterd numbers reminder is =" << num % num2;
+
+    getch();
+}
