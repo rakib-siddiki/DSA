2b888fac385f00a3614d91f82dfae7b036e6f84c
rakib-siddiki <rmrakib901@gmail.com>
2024-11-03 08:13:44 +0600

    D_Fixed_Password

---
+++ Module_6_Practice/D_Fixed_Password.c
@@ -0,0 +1,32 @@
+
+// Given multiple lines each line contains a number X which is a password. Print "Wrong" if the password is incorrect otherwise, print "Correct" and terminate the program.
+
+// Note: The "Correct" password is the number 1999.
+
+// Input
+// The input contains several passwords.
+
+// Each line contains a number X (10^3 ≤ X ≤ 10^4 - 1).
+
+// Output
+// Print "Wrong" if the password is typed wrong otherwise, print "Correct" if the password is typed correctly.
+
+#include <stdio.h>
+int main()
+{
+    int x;
+    while (scanf("%d", &x) != EOF)
+    {
+        if (x == 1999)
+        {
+            printf("Correct\n");
+            break;
+        }
+        else
+        {
+            printf("Wrong\n");
+        }
+    }
+
+    return 0;
+}
