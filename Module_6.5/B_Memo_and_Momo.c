475de0159d6cb4d21f7593e76701a2ab5bd44c9f
rakib-siddiki <rmrakib901@gmail.com>
2024-11-04 18:44:16 +0600

    B_Memo_and_Momo

---
+++ Module_6.5/B_Memo_and_Momo.c
@@ -0,0 +1,58 @@
+// Memo and Momo are playing a game. Memo will choose a positive number a
+// , and Momo will choose a positive number b
+// .
+
+// Your task is to tell them who will win according to the following rules:
+
+// If both a
+//  and b
+//  are divisible by k
+// , both of them win and you should print "Both".
+// If a
+//  is divisible by k
+//  but b
+//  isn't, Memo wins and you should print "Memo".
+// If b
+//  is divisible by k
+//  but a
+//  isn't, Momo wins and you should print "Momo".
+// If both a
+//  and b
+//  are not divisible by k
+// , no one wins and you should print "No One".
+// Input
+// Only one line containing three positive numbers a
+// , b
+//  and k
+//  (1≤a,b,k≤1018
+// ).
+
+#include <stdio.h>
+int main()
+{
+    long long a, b, k;
+    scanf("%lld %lld %lld", &a, &b, &k);
+
+    // Check divisibility conditions
+    int a_divisible = (a % k == 0);
+    int b_divisible = (b % k == 0);
+
+    if (a_divisible && b_divisible)
+    {
+        printf("Both");
+    }
+    else if (a_divisible)
+    {
+        printf("Memo");
+    }
+    else if (b_divisible)
+    {
+        printf("Memo");
+    }
+    else
+    {
+        printf("No One");
+    }
+
+    return 0;
+}
