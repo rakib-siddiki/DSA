1b0568b8fc4121db435783327fd88a492ab7e3bb
rakib-siddiki <rmrakib901@gmail.com>
2024-11-05 18:45:53 +0600

    F_Reversing

---
+++ Module_7.5/F_Reversing.c
@@ -0,0 +1,31 @@
+// Given a number N and an array A of N numbers. Print the array in a reversed order.
+
+// Note:
+
+// *Don't use built-in-functions.
+
+// Input
+// First line contains a number N (1 ≤ N ≤ 103) number of elements.
+
+// Second line contains N numbers (0 ≤ Ai ≤ 109).
+
+// Output
+// Print the array in a reversed order.
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
